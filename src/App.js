edabb75affaa2715ba0220f86beed25dc325406f
Rinzler007 <harshdas562@gmail.com>
2022-09-16 17:52:55 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import BoardView from './components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2 style={{textAlign: "center", color: "#fff"}}>
+          Welcome to the Game of 2048
+        </h2>
+        <br />
+      </div>
+      <BoardView/>
+      <div>
+        <br />
+        <h3 style={{textAlign: "center", color: "#fff"}}>Designed by Harsh</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
